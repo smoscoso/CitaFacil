dc10e5f4c9af2b48b272ee3fa9d931708fb8166e
Sergio Moscoso <Sergio Moscoso@LAPTOP-JN7HOF24>
2023-09-21 20:28:44 -0500

    Agregar archivos de proyecto.

---
+++ CitaFacil/Models/Registrar_Cliente.cs
@@ -0,0 +1,15 @@
+﻿namespace CitaFacil.Models
+{
+    public class Registrar_Cliente
+    {
+        public int Id { get; set; }
+        public string Primer_Nombre { get; set; }
+        public string Segundo_Nombre { get; set; }
+        public string Primer_Apellido { get; set; }
+        public string Segundo_Apellido { get; set; }
+        public int Telefono { get; set; }
+        public int Cedula { get; set; }
+        public string Correo { get; set; }
+
+    }
+}
